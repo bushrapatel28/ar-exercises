681ef63ce51f9e6dcb84180164d3dbbf9b23f111
Bushra Patel <bpatel60@yahoo.com>
2024-09-04 17:39:20 -0400

    Relate the 2 tables and make entries for the employee table

--- exercises/exercise_6.rb
+++ exercises/exercise_6.rb
@@ -9,3 +9,11 @@ puts "Exercise 6"
 puts "----------"
 
 # Your code goes here ...
+
+@store1.employees.create(first_name: "Khurram", last_name: "Virani", hourly_rate: 60)
+@store1.employees.create(first_name: "Sam", last_name: "Johnson", hourly_rate: 50)
+@store1.employees.create(first_name: "John", last_name: "Doe", hourly_rate: 60)
+@store2.employees.create(first_name: "Potato", last_name: "Billa", hourly_rate: 90)
+@store2.employees.create(first_name: "Jane", last_name: "Smith", hourly_rate: 65)
+@store2.employees.create(first_name: "Maya", last_name: "Laila", hourly_rate: 70)
+@store2.employees.create(first_name: "Momo", last_name: "Chan", hourly_rate: 70)
