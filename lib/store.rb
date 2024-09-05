eb66997db5d16817b74d6bdcca4bcaaf6c09a3b8
Bushra Patel <bpatel60@yahoo.com>
2024-09-05 11:25:41 -0400

    Add validations and display the error messages

--- lib/store.rb
+++ lib/store.rb
@@ -1,3 +1,14 @@
 class Store < ActiveRecord::Base
   has_many :employees
+
+  validates :name, length: { minimum: 3 }
+  validates :annual_revenue, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
+  validate :mens_and_womens_apparel_cannot_be_falsy
+
+  def mens_and_womens_apparel_cannot_be_falsy
+    if !mens_apparel && !womens_apparel
+      errors.add(:mens_apparel, "AND")
+      errors.add(:womens_apparel, "cannot be false")
+    end
+  end
 end
